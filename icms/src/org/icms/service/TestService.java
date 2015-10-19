91f1ae4eb48c59c7df5844437424113108c6ba21
scott <scott@scott-PC>
2015-10-19 23:08:12 +0800

    hibernate4配置

--- icms/src/org/icms/service/TestService.java
+++ icms/src/org/icms/service/TestService.java
@@ -4,19 +4,23 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.icms.dao.TestDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
-public class TestService implements Test{
+public class TestService{
+	private Log log = LogFactory.getLog(getClass());
 	
 	@Autowired
 	private TestDao testDao;
 	
 	public void test(){
 		List<Integer> list = testDao.getSession().createSQLQuery("select bean.user_id from jc_user_role bean").list();
+		log.info("test");
 	}
 
 }
